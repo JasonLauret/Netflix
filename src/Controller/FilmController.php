66d175f26e3616d91bbe52c4d707f3dbe6eaecc8
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-06 16:12:28 +0100

    Ajout des fonctions pour afficher tous les films et de voir le detail d'un film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FilmController extends AbstractController
 {
-    #[Route('/film', name: 'app_film')]
-    public function index(): Response
+    #[Route('/film', name: 'film')]
+    public function index(FilmRepository $filmRepository): Response
     {
+        $film = $filmRepository->findAll();
         return $this->render('film/index.html.twig', [
             'controller_name' => 'FilmController',
+            'films' => $film
+        ]);
+    }
+
+    #[Route('/show/{id}', name:'show')]
+    public function show(FilmRepository $filmRepository, $id){
+        
+        $showFilm = $filmRepository->find($id);
+                    
+        return $this->render('film/show.html.twig', [
+            'showFilm' => $showFilm
         ]);
     }
 }
