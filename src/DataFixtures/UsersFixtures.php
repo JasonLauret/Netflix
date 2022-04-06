518dae382052e6bea0c3d61f57ae127e427dfce5
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-06 17:19:36 +0100

    Création de l'entité User

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class UsersFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
