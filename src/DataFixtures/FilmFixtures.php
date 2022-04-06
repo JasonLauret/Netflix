73b126631318812f3b30386b20690292ebeff122
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-06 15:53:48 +0100

    Création de 10 film avec les fixtures

---
+++ src/DataFixtures/FilmFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Film;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class FilmFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for($i = 1; $i <= 10; $i++){
+            $film = new Film();
+            $film->setTitre("Film n°$i")
+                    ->setDescription("Description du film n°$i")
+                    ->setImage("https://via.placeholder.com/300x150")
+                    ->setDuree("2h00");
+            $manager->persist($film);
+        }
+        $manager->flush();
+    }
+}
