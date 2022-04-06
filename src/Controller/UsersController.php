cd93f7f4f6acb5c034976cd99d7bb20549e30c80
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-06 19:46:13 +0100

    Affichage des utilisateurs

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
-    public function index(): Response
+    #[Route('/users', name: 'users')]
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('users/index.html.twig', [
-            'controller_name' => 'UsersController',
+            'users' => $users
         ]);
     }
 }
