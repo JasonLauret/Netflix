32f438d8a299407f5f41d2511b5d999d74c07ecb
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-07 21:53:11 +0100

    Création du formulaire et de la fonction pour ajouter et modifier un film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use App\Form\FilmType;
 use App\Repository\FilmRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,13 +23,37 @@ class FilmController extends AbstractController
         ]);
     }
 
+    #[Route('/film/new', name: 'createFilm')]
+    #[Route('film/{id}/edit', name: 'editFilm')]
+    public function form(Request $request, EntityManagerInterface $manager, Film $film = null)
+    {
+        if(!$film){
+            $film = new Film();
+        }
+        $form = $this->createForm(FilmType::class, $film);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($film);
+            $manager->flush();
+            return $this->redirectToRoute('film', [
+                'id' => $film->getId()
+            ]);
+        }
+        return $this->render('film/form.html.twig',[
+            'formFilm' => $form->createView(),
+            'editMode' => $film->getId() !== null
+        ]);
+    }
+
     #[Route('/show/{id}', name:'show')]
-    public function show(FilmRepository $filmRepository, $id){
+    public function show(FilmRepository $filmRepository, $id, Film $film){
         
         $showFilm = $filmRepository->find($id);
                     
         return $this->render('film/show.html.twig', [
-            'showFilm' => $showFilm
+            'showFilm' => $showFilm,
+            'film' => $film
         ]);
     }
 }
