86f1b7b7d4a179585e91692972b5e90b624d6402
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-04-22 17:39:02 +0100

    Ajout d'une deuxième manière pour tier par ordre alphabétique

--- public/js/filtre.js
+++ public/js/filtre.js
@@ -1,6 +1,7 @@
 const btn_filtre_alphabetique_croissant = document.getElementById("btn_filtre_croissant");
 const normale = document.getElementById("normale");
 const filtre_alphabetique_croissant = document.getElementById("filtre_croissant");
+// DIV qui va accueillir les titre 
 const titre_filtre = document.getElementById("titre_filtre");
 
 
@@ -11,25 +12,38 @@ document.addEventListener('DOMContentLoaded', function() {
     filtre_croissant.style.display = "none";
 });
 
-// Au click button "Ordre craissant" cache la section normale et affiche la section "filtre_croissant"
-btn_filtre_alphabetique_croissant.addEventListener("click", function(){
-    console.log('Trier par ordre croissant');
-    if(getComputedStyle(normale).display != "none"){
-        normale.style.display = "none";
-        filtre_alphabetique_croissant.style.display = "block";
-        btn_filtre_alphabetique_croissant.style.display = "none";
-        // // Mettre un titre quand un filtre est activé
-        // if(getComputedStyle(filtre_croissant).display != "none"){
-        //     const titre_filtre_croissant = document.createElement("h3");
-        //     titre_filtre_croissant.innerHTML = '<h3 class="fs-1">Trier par ordre croissant</h3>';
-        //     // titre_filtre_croissant.classList.add("border");
-        //     titre_filtre.appendChild(titre_filtre_croissant);
-        // }
-    } else {
-        normale.style.display = "block";
-        filtre_alphabetique_croissant.style.display = "none";
+// Filtre trie par ordre alphabétique
+
+    // // Premiere manière
+    // // Au click button "Ordre craissant" cache la section normale et affiche la section "filtre_croissant"
+    // btn_filtre_alphabetique_croissant.addEventListener("click", function(){
+    //     console.log('Trier par ordre croissant');
+    //     if(getComputedStyle(normale).display != "none"){
+    //         normale.style.display = "none";
+    //         filtre_alphabetique_croissant.style.display = "block";
+    //         btn_filtre_alphabetique_croissant.style.display = "none";
+    //         // // Mettre un titre quand un filtre est activé
+    //         // if(getComputedStyle(filtre_croissant).display != "none"){
+    //         //     const titre_filtre_croissant = document.createElement("h3");
+    //         //     titre_filtre_croissant.innerHTML = '<h3 class="fs-1">Trier par ordre croissant</h3>';
+    //         //     // titre_filtre_croissant.classList.add("border");
+    //         //     titre_filtre.appendChild(titre_filtre_croissant);
+    //         // }
+    //     } else {
+    //         normale.style.display = "block";
+    //         filtre_alphabetique_croissant.style.display = "none";
+    //     }
+    // });
+
+    // Deuxième manière: ajout de  ( onClick="trieAlphabetiqueCroissant()" ) en attibut du bouton "Ordre alphabétique"
+    function trieAlphabetiqueCroissant() {
+        if(getComputedStyle(normale).display != "none"){
+            normale.style.display = "none";
+            filtre_alphabetique_croissant.style.display = "block";
+            btn_filtre_alphabetique_croissant.style.display = "none";
+        } else {
+            normale.style.display = "block";
+            filtre_alphabetique_croissant.style.display = "none";
+        }
     }
-});
-
-
-
+//
