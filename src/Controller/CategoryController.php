919cc679b572576067d44f24e31d22a2799e2cae
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-05-11 17:54:56 +0100

    Création de la fonction pour ajouter ou modifier des category et les bouton associé

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +20,31 @@ class CategoryController extends AbstractController
 
         return $this->render('category/index.html.twig', [
             'categorys' => $category,
+        ]);
+    }
+
+    #[Route('a/category/new', name: 'createCategory')]
+    #[Route('a/category/{id}/edit', name: 'editCategory')]
+    public function form(Request $request, EntityManagerInterface $manager, Category $category = null)
+    {
+        if(!$category){
+            $category = new Category();
+        }
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($category);
+            $manager->flush();
+    
+            return $this->redirectToRoute('category', [
+                'id' => $category->getId()
+            ]);
+        }
+        return $this->render('category/form.html.twig',[
+            'formCategory' => $form->createView(),
+            'editMode' => $category->getId() !== null
         ]);
     }
 }
