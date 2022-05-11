c753bd9d54211591e0fb65381fe0fa175f3e5ea8
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-05-11 17:39:21 +0100

    Création de la fonction pour afficher toute les category et ajout du bouton associé dans la navbar

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $category = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categorys' => $category,
+        ]);
+    }
+}
