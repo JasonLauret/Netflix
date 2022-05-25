88e5610ccdfb87e23e61b7364f65c6bf707d26b3
JasonLauret <80036076+JasonLauret@users.noreply.github.com>
2022-05-25 14:55:53 +0100

    Création d'une section 'Films mieux notés'sur la homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(TranslatorInterface $translator): Response
+    public function index(): Response
     {
-        $message = $translator->trans('Welcome to my site');
-        $this->addFlash('message', $message);
-
         return $this->render('home/index.html.twig', [
-            
+
         ]);
     }
 }
